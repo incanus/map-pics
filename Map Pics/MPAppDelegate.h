c4f872c42d016ed3f6b19876227050e9e54b2c72
Justin R. Miller <incanus@codesorcery.net>
2013-07-31 10:12:57 -0400

    Initial Commit

---
+++ Map Pics/MPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPAppDelegate.h
+//  Map Pics
+//
+//  Created by Justin R. Miller on 7/31/13.
+//  Copyright (c) 2013 MapBox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MPAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Map Pics/MPAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MPAppDelegate.m
+//  Map Pics
+//
+//  Created by Justin R. Miller on 7/31/13.
+//  Copyright (c) 2013 MapBox. All rights reserved.
+//
+
+#import "MPAppDelegate.h"
+
+@implementation MPAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
